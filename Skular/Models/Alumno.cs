521302d6e7bd7de4837dd6db438b441ce39398f6
Julio Cesar Torres Cama <julio.torres.c@tecsup.edu.pe>
2019-03-28 14:33:35 -0500

    Configure connection string, Add domain classes Representante and Alumno

---
+++ Skular/Models/Alumno.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Skular.Models
+{
+    public class Alumno
+    {
+        public int Ciscolar { get; set; }
+        public int Cedularepresentante { get; set; }
+        public virtual Representante Representante { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Direccion { get; set; }
+        public int Telefono { get; set; }
+        public int Sexo { get; set; }
+    }
+}
---
+++ Skular/Models/Representante.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Skular.Models
+{
+    public class Representante
+    {
+        public int Cedula { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string Parentesco { get; set; }
+        public int telefono { get; set; }
+        public ICollection<Alumno> Alumnos { get; set; }
+    }
+}
