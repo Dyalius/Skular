4aa74263149d9d8d9359d89544254bcda299fc6e
Julio Cesar Torres Cama <julio.torres.c@tecsup.edu.pe>
2019-03-28 14:14:10 -0500

    Empty MVC/WebAPI VS2019 .NET 4.7.2

---
+++ Skular/Areas/HelpPage/ModelDescriptions/EnumValueDescription.cs
@@ -0,0 +1,11 @@
+namespace Skular.Areas.HelpPage.ModelDescriptions
+{
+    public class EnumValueDescription
+    {
+        public string Documentation { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
---
+++ Skular/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Skular.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
